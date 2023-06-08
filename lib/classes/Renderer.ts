6ab7a3d4d7083ea24c61ee6b1b1e02e702e74026
Pavel Sapachev <pavel@sapachev.com>
2023-06-09 00:21:32 +0300

    fix: lint

--- lib/classes/Renderer.ts
+++ lib/classes/Renderer.ts
@@ -8,7 +8,7 @@ export class Renderer {
   static renderTemplate(template: string, vars: TemplateVariables) {
     if (TYPOGRAF_LOCALES.has(vars.locale)) {
       const tp = new Typograf({ locale: [TYPOGRAF_LOCALES.get(vars.locale)], htmlEntity: { type: "name" } });
-      for (let prop in vars) {
+      for (const prop in vars) {
         vars[prop] = tp.execute(vars[prop]);
       }
     }
