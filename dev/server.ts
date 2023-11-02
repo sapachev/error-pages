814c20c697faf8efb5fde63fd511bce60d91fab4
Pavel Sapachev <pavel@sapachev.com>
2023-11-02 23:40:19 +0300

    feat: add hot-reload over sse

--- dev/server.ts
+++ dev/server.ts
@@ -1,6 +1,9 @@
 import "reflect-metadata";
 
+import chokidar from "chokidar";
+import { readFileSync } from "fs";
 import Koa from "koa";
+import Stream from "stream";
 
 import { ICompiler } from "../lib/classes/Compiler";
 import { IFileSystemHelper } from "../lib/classes/FileSystemHelper";
@@ -33,13 +36,46 @@ fsHelper.readConfig(pr.get("config")).then(async (config) => {
   // Server setup
   const app = new Koa();
 
+  const watcher = chokidar.watch([`${pr.get("src")}/**`, `${pr.get("theme")}/**`], {
+    persistent: true,
+  });
+
+  // Hot-reload feature over Server-sent events (SSE)
+  app.use((ctx, next) => {
+    if ("/events" == ctx.path) {
+      ctx.set({
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+      });
+      ctx.status = 200;
+
+      const stream = new Stream.PassThrough();
+      ctx.body = stream;
+
+      stream.write("data: init\n\n");
+
+      const sseHandler = (path) => {
+        stream.write("data: reload (" + path + ")\n\n");
+      };
+
+      watcher.on("add", sseHandler).on("change", sseHandler).on("unlink", sseHandler).on("addDir", sseHandler).on("unlinkDir", sseHandler);
+
+      ctx.req.on("close", () => {
+        stream.end();
+      });
+    } else {
+      return next();
+    }
+  });
+
   // Read template in a root
   app.use(async (ctx, next) => {
     if ("/" == ctx.path) {
       try {
         let template = await fsHelper.readFile(pr.join("theme", "template.html"));
         ctx.body = template;
-        await next();
+        return next();
       } catch (_) {
         ctx.status = 500;
         ctx.body = Messages.text(MessagesEnum.NO_TEMPLATE_CONTENT);
@@ -52,7 +88,7 @@ fsHelper.readConfig(pr.get("config")).then(async (config) => {
   // Inject development variables
   app.use(async (ctx, next) => {
     if (ctx.body) {
-      ctx.body = ctx.body.replace(/<\/(head|body)>/gi, "{{ $1 }}</$1>");
+      ctx.body = ctx.body.replace(/<\/(head|body)>/gi, "{{ $1-injection }}</$1>");
       await next();
     } else {
       ctx.status = 204;
@@ -70,17 +106,18 @@ fsHelper.readConfig(pr.get("config")).then(async (config) => {
         const statusVars = await fsHelper.readJson<TemplateVariables>(pr.join("src", `${config.devCode}.json`));
 
         const devVars = {
-          head: `<script src="https://cdn.tailwindcss.com/3.2.4"></script>`,
+          "head-injection": `<script src="https://cdn.tailwindcss.com/3.2.4"></script>`,
+          "body-injection": readFileSync("./dev/sse.html").toString(),
         };
 
         if (await fsHelper.ensure(pr.get("themeConfig"))) {
           const tailwindConfig = require(pr.get("themeConfig"));
-          devVars.head += `<script>tailwind.config = ${JSON.stringify(tailwindConfig)};</script>`;
+          devVars["head-injection"] += `<script>tailwind.config = ${JSON.stringify(tailwindConfig)};</script>`;
         }
 
         if (await fsHelper.ensure(pr.get("themeCss"))) {
           const mainCss = await fsHelper.readFile(pr.get("themeCss"));
-          devVars.head += `<style type="text/tailwindcss">${mainCss}</style>`;
+          devVars["head-injection"] += `<style type="text/tailwindcss">${mainCss}</style>`;
         }
 
         ctx.body = Renderer.renderTemplate(ctx.body, { ...initVars, ...commonVars, ...statusVars, ...devVars, code: config.devCode });
